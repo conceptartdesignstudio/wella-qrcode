b14082bf832868e9a594ad134b95eb57a3771be0
Marilia Cimali da Costa Camara Caldas <marilia@ibm.com>
2025-07-24 09:24:10 -0300

    chore: new approach for images

--- src/features/group/getGroupData.ts
+++ src/features/group/getGroupData.ts
@@ -1,5 +1,4 @@
 import { api } from '@/lib/api'
-import { getMediaUrl } from '@/features/general/getMediaUrl'
 
 export const endpoint = `${process.env.NEXT_PUBLIC_WELLA_ENDPOINT}`
 
@@ -12,21 +11,20 @@ async function getProductData(productId: number, groupSlug: string) {
     ingredients: product.acf?.ingredients || '',
     slug: product.slug,
     productUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/grupo/${groupSlug}/${product.slug}`,
-    image:
-      (await getMediaUrl(product.acf?.product_group_image)) ||
-      (await getMediaUrl(product.acf?.product_image))
+    image: product.acf?.product_group_image || product.acf?.product_image_url
   }
 }
 
 export async function getGroupData(slug: string) {
-  const { data } = await api.get(`/grupos?slug=${encodeURIComponent(slug)}`)
+  const { data } = await api.get(
+    `wp/v2/grupos?slug=${encodeURIComponent(slug)}`
+  )
   if (!Array.isArray(data) || !data.length) {
     console.warn('Grupo não encontrado:', data)
     throw new Error('Grupo não encontrado')
   }
 
   const g = data[0]
-  const groupImageUrl = await getMediaUrl(g.acf.group_image)
   const relatedProducts = await Promise.all(
     (g.acf?.group_related_products || []).map((id: number) =>
       getProductData(id, slug)
@@ -37,7 +35,7 @@ export async function getGroupData(slug: string) {
     id: g.id,
     title: g.title.rendered,
     slug: g.slug,
-    image: groupImageUrl,
+    image: g.acf?.group_image_url,
     pageTitle: g.acf?.group_title || g.title.rendered,
     colors: {
       backgroundColor: g.acf?.group_background_color || '#fff',
